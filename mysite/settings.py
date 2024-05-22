e90558b21463b5eeda08519bb38485bd9a278f92
AbAyvazyan333 <abayvazyan333@gmail.com>
2024-05-22 13:42:47 +0400

    added cors options

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,6 +27,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['spacex-py.onrender.com', 'localhost']
 APPEND_SLASH = True
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
 
 # Application definition
 
@@ -38,7 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crudapp',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware','
 ]
 
 ROOT_URLCONF = 'mysite.urls'
